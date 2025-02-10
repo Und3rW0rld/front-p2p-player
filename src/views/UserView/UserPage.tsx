3a037050dd0b54d205f104f2a7b37b6ffabb16c3
Jacobo Ochoa <jochoara@unal.edu.co>
2025-02-10 17:35:24 -0500

    view: top songs component and modifications to user profile

--- src/views/UserView/UserPage.tsx
+++ src/views/UserView/UserPage.tsx
@@ -3,8 +3,11 @@ import BannerMockImg from '../../assets/img/userPageBannerMock.gif'
 import UserProfMockImg from '../../assets/img/userProfilePicMock.jpg'
 import EditButtonSVG from "../../assets/icons/edit-pwd.svg"
 
+import TopSongs from "./components/TopSongs.tsx"; 
+
 var userName:string = "YOUR_USERNAME";
 var userDescription = "Me gusta el pop polaco"
+
 
 const UserPage = () => {
 
@@ -13,21 +16,27 @@ const UserPage = () => {
         <div className = "user-page-container">
             <div className="user-profile-container">
                 <div className="user-profile-left">
-                    <img src = {EditButtonSVG}/>
-                <div className = "user-data">
-                    <img src= {UserProfMockImg} alt = "Profile picture"/>
-                    <h2 className="user-name"> {userName}</h2>
-                    <h3 className="user-description">{userDescription}</h3>
-                </div>
+                    <img src = {EditButtonSVG} className="edit-icon"/>
+                    <div className = "user-data">
+                        <img src= {UserProfMockImg} alt = "Profile picture"/>
+                        <h2 className="user-name"> {userName}</h2>
+                        <h3 className="user-description">{userDescription}</h3>
+                    </div>
+                    <div className="edit-icon"></div>
                 </div>
                 <div className="user-profile-right">
                     <img src= {BannerMockImg} alt = "Banner"/>
                 </div>
             </div>
-            <div className="top-user-songs">
-                
+            <div className="top-songs-container">
+                <h1 >
+                    {userName} TOP 3 SONGS
+                    <img src = {EditButtonSVG} className="edit-icon"/>
+                </h1>
+                <TopSongs />
             </div>
         </div>
+        
         </>
     )
 }
