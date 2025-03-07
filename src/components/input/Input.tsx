341c67a3f0c71b6f99fdee8e61a2b828ecbe1c86
Santiago Sosa <ssosag@unal.edu.co>
2025-03-07 00:33:26 -0500

    feat: add SettingsView component for user profile management; update button and input components to support new features

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,17 +1,19 @@
 import './input.css'
 import HidePwd from "../../assets/icons/hide-pwd.svg"
 import ShowPwd from "../../assets/icons/show-pwd.svg"
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 interface InputProps {
   label: string;
   placeholder: string;
   is_password: boolean;
+  inputNativeProps?: React.InputHTMLAttributes<HTMLInputElement>;
+
 }
 
 const Input = ({ label, placeholder, is_password }: InputProps) => {
 
-  const id = label.replace(/\s+/g, '-').toLowerCase()+Math.random().toString(36).substring(7);
+  const id = label.replace(/\s+/g, '-').toLowerCase() + Math.random().toString(36).substring(7);
 
   const [showPassword, setShowPassword] = useState(false);
 
@@ -20,23 +22,23 @@ const Input = ({ label, placeholder, is_password }: InputProps) => {
     document.getElementById(id)?.setAttribute('type', showPassword ? 'password' : 'text');
   }
 
-  
+
   return (
     <>
-    <div className='input-container'>
+      <div className='input-container'>
         <label htmlFor={id}>{label}</label>
         <div className='input'>
-        <input
-        type={is_password ? 'password' : 'text'}
-        id={id}
-        placeholder={placeholder}
-        />{
-            is_password && <img onClick={ handleShowPwd } draggable="false" className='icon' src={ showPassword ? ShowPwd : HidePwd } alt="Hide password" /> 
-        }
-        
+          <input
+            type={is_password ? 'password' : 'text'}
+            id={id}
+            placeholder={placeholder}
+          />{
+            is_password && <img onClick={handleShowPwd} draggable="false" className='icon' src={showPassword ? ShowPwd : HidePwd} alt="Hide password" />
+          }
+
         </div>
-        
-    </div>
+
+      </div>
     </>
   )
 }
